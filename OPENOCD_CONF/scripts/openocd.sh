ab7c40a1b91a3b3e5ca23d7e50574a8df3264cc0
boffkolt <boffkolt@gmail.com>
2016-10-25 14:13:28 +0300

    1

--- OPENOCD_CONF/scripts/openocd.sh
+++ OPENOCD_CONF/scripts/openocd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/engineer/GIT/OPENOCD_CONF/JLINK+STM32F10X
+cd /home/engineer/GIT/openocd/OPENOCD_CONF/JLINK+STM32F10X
 RUNCMD="sudo openocd"
 xterm -fg green -bg black -geometry 100x24-0-0 -e $RUNCMD &
